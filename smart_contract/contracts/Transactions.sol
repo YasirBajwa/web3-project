f2ef165e0a3e60818fbd32d85c9e70cdfbb850c0
YasirBajwa <30441410+YasirBajwa@users.noreply.github.com>
2022-07-06 21:02:26 +0500

    develop smart contract

--- smart_contract/contracts/Transactions.sol
+++ smart_contract/contracts/Transactions.sol
@@ -4,11 +4,11 @@ pragma solidity ^0.8.0;
 contract Transactions {
     uint256 transactionCount ;
 
-    event Transfer(address from , address reciever,uint amount, string message , uint256 timestamp , string keyword);
+    event Transfer(address from , address receiver,uint amount, string message , uint256 timestamp , string keyword);
 
     struct TransferStruct {
         address sender;
-        address reciever;
+        address receiver;
         uint amount;
         string message;
         uint256 timestamp;
@@ -17,18 +17,21 @@ contract Transactions {
 
     TransferStruct[] transactions;
 
-    function addToBlockChain(address payable reciever , uint amount,string memory message , string memory keyword) public{
+    function addToBlockChain(address payable receiver , uint amount,string memory message , string memory keyword) public{
         transactionCount += 1 ;
+        transactions.push(TransferStruct(msg.sender, receiver , amount ,message,block.timestamp,keyword));
+
+        emit Transfer(msg.sender, receiver , amount ,message,block.timestamp,keyword);
 
     }
 
      function getAllTransactions() public view returns(TransferStruct[] memory){
-         //return transactions
+         return transactions;
 
     }
 
      function getTransctionsCount() public view returns(uint256){
-       // return transcation count
+       return transactionCount;
 
     }
 }
