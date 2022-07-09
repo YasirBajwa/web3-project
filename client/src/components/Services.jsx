3b979483405d85477a3b42370733ca1fb60e2442
YasirBajwa <30441410+YasirBajwa@users.noreply.github.com>
2022-07-09 13:06:18 +0500

    completed service component

--- client/src/components/Services.jsx
+++ client/src/components/Services.jsx
@@ -3,10 +3,16 @@ import { BsFillShieldFill, BsShieldFillCheck } from "react-icons/bs";
 import { BiSearchAlt } from "react-icons/bi";
 import { RiHeart2Fill } from "react-icons/ri";
 
-const ServiceCard = ({color,title,icon,subtitle}) => (
+const ServiceCard = ({ color, title, icon, subtitle }) => (
   <div className="flex flex-row justify-start items-center white-glassmorphism p-3 m-2 cursor-pointer hover:shadow-xl">
-    <div className={`w-10 h-10 rounded-full flex justify-center items-center ${color}`}>
-       {icon}
+    <div
+      className={`w-10 h-10 rounded-full flex justify-center items-center ${color}`}
+    >
+      {icon}
+    </div>
+    <div className="ml-5 flex flex-col flex-1">
+      <h3 className="mt-2 text-white text-lg">{title}</h3>
+      <p className="mt-1 text-white text-sm md:w-9/12">{subtitle}</p>
     </div>
   </div>
 );
@@ -25,9 +31,21 @@ const Services = () => {
         <ServiceCard
           color="bg-[#2952E3]"
           title="Security Gurantee"
-          icon={<BsShieldFillCheck fontSize={21} className="text-white"
-          subtitle="Lorem text dummy text font"
-          />}
+          icon={<BsShieldFillCheck fontSize={21} className="text-white" />}
+          subtitle="Security is guranteed. We always maintain privacy and maintain the quality of our products"
+
+        />
+        <ServiceCard
+          color="bg-[#8945F8]"
+          title="Best exchange rates"
+          icon={<BiSearchAlt fontSize={21} className="text-white" />}
+          subtitle="Security is guranteed. We always maintain privacy and maintain the quality of our products"
+        />
+        <ServiceCard
+          color="bg-[#F84550]"
+          title="Fastest transactions"
+          icon={<RiHeart2Fill fontSize={21} className="text-white" />}
+          subtitle="Security is guranteed. We always maintain privacy and maintain the quality of our products"
         />
       </div>
     </div>
